ecc7fa2861ba17a3e6744ac9d6fb803524526608
amankumar-dev <rjvkha@gmail.com>
2026-03-30 20:44:48 +0530

    'basics'

---
+++ python_fundamentals/basics/basics.py
@@ -0,0 +1,19 @@
+# 1) What is Python and how is it interpreted?
+# -> Python is a high level programming language which is founded by Guido Van Rossum and it is interpreted because it first convert source code -> Bytecode -> then VM convert it into Machine Code.
+print('Hello Aman')
+
+# 2) What are variables and dynamic typing?
+x=10 # Variable which refer to the value
+x='aman' # Type of variable changed during runtime i.e. it refers now string object from int object
+
+# 3) Difference between list, tuple, set, dictionary?
+a=[10,20,30,40,10]          # List -> Mutable (can modify)
+b=(10,20,30,40,10)          # Tuple -> Immutable (can't modify)
+c={10,20,30,40}             # Set -> Mutable and unique value store
+d={'id':1,'name':'aman'}    # dict -> Mutable and Store in key-value pair
+
+# 4) When would you use tuple over list?
+# -> We use tuple over list only when we want that and already aware that we don't need to modify the data
+
+# 5) What is mutability?
+# -> Which allow to change the data, term which refer the change or modify
